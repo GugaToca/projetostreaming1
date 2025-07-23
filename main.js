42e37e172599a792b8e6a869d302cafe233f067e
GugaToca <gustavotocalino08@outlook.com>
2025-07-23 17:56:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", () => {
+  console.log("NOVAMÍDIA carregada com sucesso");
+});
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    if (i === index) slide.classList.add("active");
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 5000); // troca a cada 5 segundos
